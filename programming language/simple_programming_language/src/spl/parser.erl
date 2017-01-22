9d9da6c73ad9e45af22278597b8fea93890f07dc
Mateusz Jarzyna <mateusz.jarzyna@jtendo.com>
2017-01-22 10:38:53 +0100

    call function

--- programming language/simple_programming_language/src/spl/parser.erl
+++ programming language/simple_programming_language/src/spl/parser.erl
@@ -2,21 +2,23 @@
 
 -export([run/3]).
 
+-define(ACC_FUN, acctual_function).
+
 run(ClassToRun, FunToRun, Args) ->
+    put(?ACC_FUN, {ClassToRun, FunToRun}),
     {ArgsNames, Operations} = spl_manager:get_function(ClassToRun, FunToRun),
     parse_fun(Operations, ArgsNames, Args).
 
 parse_fun(Operations, ArgsNames, ArgsToSet) ->
-	Vars = #{},
-	Vars2 = set_args(ArgsNames, ArgsToSet, Vars),
-    {FunctionResult, _LastVars} = parse(Operations, Vars2),
+	Vars = set_args(ArgsNames, ArgsToSet, #{}),
+    {FunctionResult, _LastVars} = parse(Operations, Vars),
     FunctionResult.
 
 parse(Operations, Vars) when is_list(Operations) ->
     lists:foldl(fun(Operation, {_LastRes, AccVars}) ->
                     parse(Operation, AccVars)
                 end,
-                {true, Vars},
+                {false, Vars},
                 Operations
                 );
 parse({if_else, Expression, WhenTrue, WhenFalse}, Vars) ->
@@ -30,6 +32,11 @@ parse({greater_than, Left, Right}, Vars) ->
     {ParsedRight, Vars3} = parse(Right, Vars2),
     Value = ParsedLeft > ParsedRight,
     {Value, Vars3};
+parse({call_self_function, FunName, Args}, Vars) ->
+    {AccClass, AccFun} = get(?ACC_FUN),
+    RunRes = run(AccClass, FunName, Args),
+    put(?ACC_FUN, {AccClass, AccFun}),
+    {RunRes, Vars};
 parse({assign_variable, Name, Value}, Vars) ->
     {Parsed, Vars2} = parse(Value, Vars),
     Vars3 = assign_variable(Name, Parsed, Vars2),
@@ -40,8 +47,9 @@ parse({string, String}, Vars) ->
     {String, Vars};
 parse({number, Number}, Vars) ->
     {Number, Vars};
-parse(UnknownOp, _Vars) ->
-	io:format("Unknown operation ~p\n", [UnknownOp]).
+parse(UnknownOp, Vars) ->
+	io:format("Unknown operation ~p\n", [UnknownOp]),
+    {false, Vars}.
 
 set_args([] = _FunArgsNames, [] = _ArgsToSet, Vars) ->
 	Vars;
